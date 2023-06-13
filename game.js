0ae45d40831a0592b384c0c068c1ca0e868009be
Jrobinson212 <134180382+Jrobinson212@users.noreply.github.com>
2023-06-12 20:35:21 -0500

    psuedo code

--- game.js
+++ game.js
@@ -1,1 +1,35 @@
-console.log("Hello RPS")
+//variables
+    // num wins
+    // num losses
+    // num ties
+
+
+
+// p[ayers choose R, P , S
+    //human choice - get choice from user input
+    //computer randomly chooses 
+
+// compare choices
+    //if humanChoice is S, and computerChoice is P
+    //if the humanChoice is R ans computerChoice is S OR 
+    //if humanChoice is R and computerChoice is S OR
+    //if humanChoice is P and computerChoice is R
+        //alert that we win
+        //increase wins
+    
+    //if humanChoice === computerChoice
+
+        //alert that we ties- 
+     //increse ties
+
+    //else
+    //alert that welost
+    //increase losses
+
+    //ask user "do you want to play again?"
+        // if yes
+            //restart game
+    //if no
+            //exit game
+
+
