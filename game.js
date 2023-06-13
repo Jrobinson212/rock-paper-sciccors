611842b9fabcdf335f1a39e723fd42b631dfc9c6
Jrobinson212 <134180382+Jrobinson212@users.noreply.github.com>
2023-06-12 20:58:26 -0500

    humanChoice and randmNUm

--- game.js
+++ game.js
@@ -1,13 +1,21 @@
 //variables
-    // num wins
-    // num losses
-    // num ties
+    var  wins = 0;
+    var  losses = 0;
+    var  ties = 0;
+    
+    var choices = ["R", "P", "S"]
 
-
+// alert, confirm, prompt
 
 // p[ayers choose R, P , S
+
     //human choice - get choice from user input
+    var humanChoice = prompt("choose R, P or S")
+    console.log(humanChoice)
+
     //computer randomly chooses 
+    var randomNum = Math.floor(Math.random() * 3)
+    console.log(choices[randomNum])
 
 // compare choices
     //if humanChoice is S, and computerChoice is P
