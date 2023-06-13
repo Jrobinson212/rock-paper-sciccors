0c032bf312a1d17f4c5b97b83936eb4dd2512076
Jrobinson212 <134180382+Jrobinson212@users.noreply.github.com>
2023-06-12 21:18:31 -0500

    scoring

--- game.js
+++ game.js
@@ -9,28 +9,47 @@
 
 // p[ayers choose R, P , S
 
-    //human choice - get choice from user input
+    //human choice
     var humanChoice = prompt("choose R, P or S")
     console.log(humanChoice)
 
-    //computer randomly chooses 
+    //random computer choice
     var randomNum = Math.floor(Math.random() * 3)
-    console.log(choices[randomNum])
-
+    var computerChoices = choices[randomNum]
+ console.log("Human: " + humanChoice)
+ console.log("Compter: " + computerChoices)
 // compare choices
-    //if humanChoice is S, and computerChoice is P
-    //if the humanChoice is R ans computerChoice is S OR 
-    //if humanChoice is R and computerChoice is S OR
+
+if ( 
+ humanChoice === "S" && computerChoices === "P" ||
+ humanChoice === "R" && computerChoices === "S" || 
+ humanChoice === "p" && computerChoices === "R" 
+
+) { alert("You won") 
+    wins++
+
+} else if (humanChoice === computerChoices){
+alert("Round tied") 
+    ties++
+}
+    
     //if humanChoice is P and computerChoice is R
         //alert that we win
         //increase wins
     
-    //if humanChoice === computerChoice
+    //if 
 
-        //alert that we ties- 
-     //increse ties
+    
 
-    //else
+    else {
+        alert ("You lost")
+        losses++
+    }
+    
+    //show scores
+    alert("Wins: " + wins + "\nTies: " + ties + "\nLosses: " + losses)
+
+
     //alert that welost
     //increase losses
 
