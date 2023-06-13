b66d61aa9875cd35ef640383c6ab4a22c82978f7
Jrobinson212 <134180382+Jrobinson212@users.noreply.github.com>
2023-06-12 21:33:00 -0500

    while loop

--- game.js
+++ game.js
@@ -5,58 +5,58 @@
     
     var choices = ["R", "P", "S"]
 
-// alert, confirm, prompt
-
-// p[ayers choose R, P , S
-
+    var keepPlaying = true
+    while (keepPlaying) {
+        
     //human choice
-    var humanChoice = prompt("choose R, P or S")
-    console.log(humanChoice)
+        var humanChoice = prompt("Choose R, P or S")
+        console.log(humanChoice)
 
-    //random computer choice
-    var randomNum = Math.floor(Math.random() * 3)
-    var computerChoices = choices[randomNum]
- console.log("Human: " + humanChoice)
- console.log("Compter: " + computerChoices)
-// compare choices
+        //random computer choice
+        var randomNum = Math.floor(Math.random() * 3)
+        var computerChoices = choices[randomNum]
+    console.log("Human: " + humanChoice)
+    console.log("Compter: " + computerChoices)
+    // compare choices
 
-if ( 
- humanChoice === "S" && computerChoices === "P" ||
- humanChoice === "R" && computerChoices === "S" || 
- humanChoice === "p" && computerChoices === "R" 
+    if ( 
+    humanChoice === "S" && computerChoices === "P" ||
+    humanChoice === "R" && computerChoices === "S" || 
+    humanChoice === "p" && computerChoices === "R" 
 
-) { alert("You won") 
-    wins++
+    ) { alert("You won") 
+        wins++
 
-} else if (humanChoice === computerChoices){
-alert("Round tied") 
-    ties++
-}
-    
-    //if humanChoice is P and computerChoice is R
-        //alert that we win
-        //increase wins
-    
-    //if 
-
-    
-
-    else {
-        alert ("You lost")
-        losses++
+    } else if (humanChoice === computerChoices){
+    alert("Round tied") 
+        ties++
     }
-    
-    //show scores
-    alert("Wins: " + wins + "\nTies: " + ties + "\nLosses: " + losses)
+        
+        //if humanChoice is P and computerChoice is R
+            //alert that we win
+            //increase wins
+        
+        //if 
+
+        
+
+        else {
+            alert ("You lost")
+            losses++
+        }
+        
+        //show scores
+        alert("Wins: " + wins + "\nTies: " + ties + "\nLosses: " + losses)
 
 
-    //alert that welost
-    //increase losses
+        //alert that welost
+        //increase losses
 
-    //ask user "do you want to play again?"
-        // if yes
-            //restart game
-    //if no
-            //exit game
+        keepPlaying = confirm("Do you want to play again?")
+        
+          
+    }
+
+    alert(" See ya play again soon!")
 
 
